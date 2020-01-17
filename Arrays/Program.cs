fbed5085422b394d8017bc4df94ddb4c2ed49fed
Pavel <korvinson57@gmail.com>
2020-01-17 10:33:14 +0300

    init

---
+++ Arrays/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[5] {6, 7, 575, 55, 45};
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+
+
+        }
+    }
+}
